e0e91d90a789e7a70e86337d4b25856e3779ed02
unknown <nikolatz.t@gmail.com>
2020-05-28 14:48:44 +0200

    files itterated and added in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './assets/files.json'
+import FilesData from './assets/files.json'
+import { Table } from 'reactstrap'
 import './App.css';
 
 class App extends Component {
@@ -7,8 +8,36 @@ class App extends Component {
 
   render () {
 
+    let files = FilesData.map(data => {
+      return <tbody>
+        <tr key={data.id}>
+          <td>{data.title}</td>
+          <td>{data.number}</td>
+          <td>{data.status}</td>
+          <td>{data.autor}</td>
+          <td>{data.tip}</td>
+          <td>{data.datum}</td>
+        </tr>
+      </tbody>
+    })
+
     return (
-      <div>Hello World!!</div>
+      <div>
+        <div>Hello World!!</div>
+        <Table>
+          <thead>
+            <tr>
+              <th>Naziv dokumenta</th>
+              <th>Broj</th>
+              <th>Status</th>
+              <th>Autor</th>
+              <th>Tip</th>
+              <th>Datum</th>
+            </tr>
+          </thead>
+          {files}
+        </Table>
+      </div>
     )
   }
 }
